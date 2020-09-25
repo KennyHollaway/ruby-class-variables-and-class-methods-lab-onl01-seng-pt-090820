e1d05cb55443d6e4f39cfef581f98bfca0a0be62
Kenny Hollaway <hollawaykenny007@gmail.com>
2020-09-25 22:37:27 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -20,20 +20,20 @@ class Song
   end
   
   def self.song_genres
-    @@song_genres
+    @@song_genres.uniq 
   end
   
   def self.song_artists
-    @@song_artists
+    @@song_artists.uniq 
   end
   
   def self.genre_count
     genre_count = {}
     @@genres.each do |genre| 
       if genre_count[genre]
-      genre_count[genre] += 1 
+        genre_count[genre] += 1 
       else
-      genre_count[genre] = 1
+        genre_count[genre] = 1
       end
     end
     genre_count
@@ -43,9 +43,9 @@ class Song
     artist_count = {}
     @@artists.each do |artist|
       if artist_count[artist]
-      artist_count[artist] +=1
+        artist_count[artist] +=1
       else
-      artist_count[artist] = 1
+        artist_count[artist] = 1
       end
     end
     artist_count
